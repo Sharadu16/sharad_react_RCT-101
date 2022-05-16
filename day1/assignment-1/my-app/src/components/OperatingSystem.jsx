826fac5f667461d4c65264be2234e9a8c4bafb8e
Sharadinduprakash <Sharadyadav2786@gmail.com>
2022-05-16 22:27:58 +0530

    add

---
+++ day1/assignment-1/my-app/src/components/OperatingSystem.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const OperatingSystem = () => {
+  return (
+    <div>
+        <h1>Mobile Operating System</h1>
+        <div>
+            <li>Android</li>
+            <li>Blackberry</li>
+            <li>iPhone</li>
+            <li>Windows Phone</li>
+        </div>
+    </div>
+  )
+}
+
+export default OperatingSystem
---
+++ day1/assignment-1/my-app/src/components/Parent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Manufactures from './Manufactures'
+import OperatingSystem from './OperatingSystem'
+
+const Parent = () => {
+  return (
+    <div>
+        <OperatingSystem/>
+        <Manufactures/>
+    </div>
+  )
+}
+
+export default Parent
